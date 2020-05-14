eea16d97872401a1e415a7df1df211808755a412
Terry Thompson <terrylthompsonintx@gmail.com>
2020-05-14 15:05:42 -0500

    installed inquierer

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
 
-const [name, github] = profileDataArgs;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
+//const profileDataArgs = process.argv.slice(2);
+
+//const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+//fs.writeFile('./index.html', generatePage(name, github), err => {
+//  if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//  console.log('Portfolio complete! Check out index.html to see the output!');
+//});
